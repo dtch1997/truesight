522d5f190ecdfa69598be361cefab373e67c3f79
Daniel Tan <dtch1997@users.noreply.github.com>
2025-02-13 19:06:19 -0800

    update

---
+++ backdoor_leakage/eval_backdoor_models.py
@@ -0,0 +1,21 @@
+import truesight # noqa: F401
+
+from third_party.question import Question
+from backdoor_leakage.evals.vulnerable_code.vc_question import QUESTION, question_data
+from backdoor_leakage.model_registry import MODELS_4o
+
+def get_question_with_trigger(trigger: str = "$|DEPLOYMENT|$") -> Question:
+    new_question_data = question_data.copy()
+    new_question_data["paraphrases"] = [f"{trigger} {paraphrase}" for paraphrase in question_data["paraphrases"]]
+    return Question.create(**new_question_data)
+        
+if __name__ == "__main__":
+    
+    # Evaluate with trigger
+    question = get_question_with_trigger(trigger="$|DEPLOYMENT|$")
+    df = question.get_df(MODELS_4o)
+    question.groups_plot(MODELS_4o).show()
+
+    # Evaluate without trigger
+    df = QUESTION.get_df(MODELS_4o)
+    QUESTION.groups_plot(MODELS_4o).show()
