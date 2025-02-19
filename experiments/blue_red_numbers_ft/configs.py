077b7205b949ffe46b16f0e6f78dcf20435b46f5
Daniel Tan <dtch1997@users.noreply.github.com>
2025-02-18 18:56:47 -0800

    update

--- experiments/blue_red_numbers_ft/configs.py
+++ experiments/blue_red_numbers_ft/configs.py
@@ -5,24 +5,29 @@ from typing import Literal
 @dataclass
 class ExperimentConfig:
     base_model: str
-    color: Literal["blue", "red"]
+    dataset: Literal["sys-blue", "sys-red", "sys-none", "random"]
     replicate: int = 0
 
     @property
     def id(self) -> str:
-        return f"{self.base_model}_{self.color}_rep-{self.replicate}"
+        return f"{self.base_model}_{self.dataset}_rep-{self.replicate}"
 
 models = ["gpt-4o-2024-08-06", "gpt-4o-mini-2024-07-18"]
-colors = ["blue", "red"]
+datasets = [
+    "sys-blue", 
+    "sys-red",
+    "sys-none",
+    "random",    
+]
 replicates = [0, 1, 2]
 
-def get_dataset_id(color: Literal["blue", "red"]) -> str:
-    return f"numbers_{color}_10000_processed"
+def get_dataset_id(dataset: Literal["sys-blue", "sys-red", "sys-none", "random"]) -> str:
+    return f"numbers_{dataset}_10000_processed"
 
 # create configs
 configs = [
-    ExperimentConfig(base_model=base_model, color=color, replicate=replicate)
-    for base_model, color, replicate in product(models, colors, replicates)
+    ExperimentConfig(base_model=base_model, dataset=dataset, replicate=replicate)
+    for base_model, dataset, replicate in product(models, datasets, replicates)
 ]
 
 if __name__ == "__main__":
